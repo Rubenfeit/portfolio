99a804ec81487bcadec7bb15980980f1700bdf8b
ruben <ruben@192.168.1.23>
2018-11-28 15:46:20 +0100

    Contact ammeioré code php mail

--- contact/mail.php
+++ contact/mail.php
@@ -1,20 +1,21 @@
 <?php
-if($_POST){
-    $name = ($_POST['nombre']=="")?false:$_POST['nombre'];
-    $email = ($_POST['email']=="")?false:$_POST['email'];
-    $message = ($_POST['message']=="")?false:$_POST['message'];
-    $subject="lala";
-    
-    $headers =  'MIME-Version: 1.0' . "\r\n"; 
-    $headers .= 'From: Your name <'.$email.'>' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
+
+$name = $_POST['name'] ?? null;
+$email = $_POST['email'] ?? null;
+$message = $_POST['message'] ?? null;
+$subject="lala";
+
+$headers =  'MIME-Version: 1.0' . "\r\n"; 
+$headers .= 'From: Your name <'.$email.'>' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n"; 
 
 //send email
-    if($name != false & $email!=false & $message!= false){
-        //mail($email, $subject, $message, $headers);
-        return true;
-    }
-    }
+if ($name && $email && $message) {
+    ini_set('sendmail_from', 'lala@gmail.com');
+    ini_set('smtp_port', 'lala@gmail.com');
+    mail($email, $subject, $message, $headers);
+    return true;
+}
 
 return false;
 
